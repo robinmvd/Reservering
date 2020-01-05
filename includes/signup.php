ae0d859805c44b596a1448c3a2c1f27e45c6aaa2
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-01-05 20:34:33 +0100

    Add check to see username is available.

--- includes/signup.php
+++ includes/signup.php
@@ -11,28 +11,61 @@ if (isset($_POST['signup-submit'])) {
     $passwordRepeat = $_POST['pwd-repeat'];
 
     //check of alle velden zijn ingevuld
-    if (empty($username) || empty($email)|| empty($password)|| empty($passwordRepeat)) {
-        header("Location: ../signup.php?error=emptyfields&username=".$username."&mail=".$email);
+    if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
+        header("Location: ../signup.php?error=emptyfields&username=" . $username . "&mail=" . $email);
         exit();
-    }
-    else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
         header("Location: ../signup.php?error=invalidnamemail");
         exit();
-    }
-    //Check of er een juist emailadres is ingevuld
+    } //Check of er een juist emailadres is ingevuld
     else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: ../signup.php?error=invalidmail&username=".$username);
+        header("Location: ../signup.php?error=invalidmail&username=" . $username);
         exit();
-    }
-    //Check of username de juiste tekens bevat
+    } //Check of username de juiste tekens bevat
     else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        header("Location: ../signup.php?error=invalidname&mail=".$email);
+        header("Location: ../signup.php?error=invalidname&mail=" . $email);
         exit();
 
-    }
-    //check of de passwords gelijk zijn aan elkaar
-    else if ($password == $passwordRepeat) {
-        header("Location: ../signup.php?error=passwordcheck&username=".$username."&mail=".$email);
+    } //check of de passwords gelijk zijn aan elkaar
+    else if ($password !== $passwordRepeat) {
+        header("Location: ../signup.php?error=passwordcheck&username=" . $username . "&mail=" . $email);
         exit();
+    } //check of username al in gebruik is
+    else {
+        $sql = "SELECT usernameUsers FROM users WHERE usernameUsers=?";
+        $statement = mysqli_stmt_init($connection);
+        if (!mysqli_stmt_prepare($statement, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($statement, "s", $username);
+            mysqli_stmt_execute($statement);
+            mysqli_stmt_store_result($statement);
+            $resultCheck = mysqli_stmt_num_rows($statement);
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken&mail=" . $email);
+                exit();
+            } else {
+                $hashedPwd = password_hash($password, PASSWORD_DEFAULT); //hashed het $password
+                //voegt de data van het signup formulier toe aan de database
+                $sql = "INSERT INTO users (usernameUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+                $statement = mysqli_stmt_init($connection);
+                if (!mysqli_stmt_prepare($statement, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($statement, "sss", $username, $email, $hashedPwd);
+                    mysqli_stmt_execute($statement);
+                    header("Location: ../signup.php?signup=succes");
+                    exit();
+                }
+            }
+        }
     }
+    mysqli_stmt_close($statement);
+    mysqli_close($connection);
+}
+else {
+    header("Location: ../signup.php");
+    exit();
 }
