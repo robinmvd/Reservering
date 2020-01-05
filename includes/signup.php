71b2710081550a725fd5fe6db9ff27f545839c4c
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-01-05 17:23:22 +0100

    Add check to see if all field are filled.

--- includes/signup.php
+++ includes/signup.php
@@ -1,6 +1,18 @@
 <?php
 
+//check of gebruiker op de sumbit knop heeft geklikt
 if (isset($_POST['signup-submit'])) {
 
     require "connection.php";
+
+    $username = $_POST['username'];
+    $email = $_POST['mail'];
+    $password = $_POST['pwd'];
+    $passwordRepeat = $_POST['pwd-repeat'];
+
+    //check of alle velden zijn ingevuld
+    if (empty($username) || empty($email)|| empty($password)|| empty($passwordRepeat)) {
+        header("Location: ../signup.php?error=emptyfields&username=".$username."&mail=".$email);
+        exit();
+    }
 }
