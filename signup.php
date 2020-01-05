41aaa872812726585192896f4b7002e067176efa
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-01-05 16:51:53 +0100

    Add html for signup form to signup page.

--- signup.php
+++ signup.php
@@ -1,2 +1,20 @@
 <?php
 
+<?php
+require "header.php";
+?>
+
+<main>
+<h1>Signup</h1>
+    <form action="includes/signup.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="mail" placeholder="E-mail">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+        <button type="submit" name="signup-submit">Signup</button>
+    </form>
+</main>
+
+<?php
+require "footer.php";
+?>
